05451d4da425ab4fb24501b6d89d6089764b1337
SatyakiB7076 <satyakit1998@gmail.com>
2024-04-09 21:20:47 +0530

    layout created inside app without functionality

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <center className="todo-container">
+      <h1>TODO App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter todo here" />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      
+        <div className="row my-3">
+          <div className="col-6">
+            Buy Milk
+          </div>
+          <div className="col-4">
+            04/04/2024
+          </div>
+          <div className="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-6">
+            Watch Anime
+          </div>
+          <div className="col-4">
+            06/04/2024
+          </div>
+          <div className="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </center>
+  );
 }
 
-export default App
+export default App;
