c003c5f27a39d2670e33589e457c71039ba51da6
SatyakiB7076 <satyakit1998@gmail.com>
2024-04-11 18:42:41 +0530

    as an obejct array placed the todo items and rendered inside the components dynamically

--- todo-app-version-two/src/App.jsx
+++ todo-app-version-two/src/App.jsx
@@ -1,18 +1,31 @@
 import AddTodo from "./Components/AddTodo";
 import Header from "./Components/Header";
-import { Todo1, Todo2 } from "./Components/Todo";
+import TodoItem from "./Components/TodoItem";
 import "./App.css";
 
 function App() {
+  const todoItems = [
+    {
+      name: "Coding",
+      dueDate: "04/04/2023",
+    },
+    {
+      name: "Watch Anime",
+      dueDate: "06/04/2023",
+    },
+    {
+      name: "Grocery",
+      dueDate: "07/04/2023",
+    },
+  ];
   return (
-    <center className="container">
+    <>
       <Header />
-      <AddTodo />
-      <div className="todo-container">
-        <Todo1 />
-        <Todo2 />
-      </div>
-    </center>
+      <center className="container">
+        <AddTodo />
+        <TodoItem todoItems={todoItems} />
+      </center>
+    </>
   );
 }
 
--- todo-app-version-two/src/Components/Todo.jsx
+++ todo-app-version-two/src/Components/Todo.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 
-function Todo1() {
-  let todoName = "Buy Milk";
-  let todoDate = "04/04/2024";
+function Todo({todoName,todoDate}) {
+
   return (
     <div className="row kg-row">
       <div className="col-6">{todoName}</div>
       <div className="col-4">{todoDate}</div>
       <div className="col-2">
-        <button type="button" class="btn btn-danger kg-button">
-          Delete
-        </button>
-      </div>
-    </div>
-  );
-}
-function Todo2() {
-  let todoName = "Watch Anime";
-  let todoDate = "06/04/2024";
-  return (
-    <div className="row kg-row">
-      <div className="col-6">{todoName}</div>
-      <div className="col-4">{todoDate}</div>
-      <div className="col-2">
-        <button type="button" class="btn btn-danger kg-button">
+        <button type="button" className="btn btn-danger kg-button">
           Delete
         </button>
       </div>
@@ -31,4 +15,5 @@ function Todo2() {
   );
 }
 
-export { Todo1, Todo2 };
+
+export default Todo;
