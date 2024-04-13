f603e94520fb6d34e4a292a47e3b38bb5436ea84
SatyakiB7076 <satyakit1998@gmail.com>
2024-04-13 13:54:09 +0530

    done modifications to the header component

--- todo-app-version-two/src/Components/Header.jsx
+++ todo-app-version-two/src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import styles from './Header.module.css'
 function Header() {
   return (
     <div>
-      <h1>TODO App</h1>
+      <h1 className={styles.toDoHeading}>TODO App</h1>
+      
     </div>
   )
 }
